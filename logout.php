d2ec2e3ae1139573a14c7d52880210cba8a21461
IlmanNurohman <2106083@itg.ac.id>
2025-06-27 10:48:46 +0700

    p

--- logout.php
+++ logout.php
@@ -3,11 +3,3 @@ session_start();
 session_unset();
 session_destroy();
 ?>
-
-<!-- Bersihkan juga localStorage via JavaScript -->
-<script>
-localStorage.removeItem('userData');
-localStorage.removeItem('user_id');
-// Arahkan ke login.html (bukan index.php agar konsisten dengan offline login)
-window.location.href = 'login.html';
-</script>
